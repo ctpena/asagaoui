beabd5bfe53c6866f2f7b4ee5dd86ae3073a8027
ctpena <git@ctpena.net>
2025-03-07 11:30:55 +0900

    Add asagaoui-icons

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,58 @@
 import gulp from 'gulp';
+import {generateFonts} from 'fantasticon';
 import sass from 'gulp-sass';
+import svgSprite from 'gulp-svg-sprite';
 import dartSass from 'sass';
 
 const sassCompiler = sass(dartSass);
+
+/**********************************************************************
+ Setup Tasks
+ Generate SVG Icons
+ **********************************************************************/
+
+function generateSVGSIcon() {
+    const config = {
+        mode: {
+            symbol: {
+                sprite: "../asagaoui-icons.svg",
+                example: true
+            }
+        }
+    };
+    return gulp.src('./assets/icons/svg/*.svg')
+        .pipe(svgSprite(config))
+        .pipe(gulp.dest('./dist/icon/'))
+}
+
+function generateIconFont() {
+    return generateFonts({
+        inputDir: './assets/icons/svg',
+        outputDir: './dist/icon',
+        name: 'asagaoui-icons',
+        prefix: 'ai',
+        fontTypes: ['woff2'],
+        assetTypes: ['css'],
+        fontsUrl: '../icon',
+        pathOptions: {
+            css: './dist/css/asagaoui-icons.css'
+        }
+    })
+        .then(results => {
+            console.log('Icon fonts generated successfully:', results);
+        })
+        .catch(err => {
+            console.error('Error generating icon fonts:', err);
+            throw err;
+        });
+}
+
+const setupTasks = gulp.parallel(generateSVGSIcon, generateIconFont);
+
+/**********************************************************************
+ default Tasks
+ compile Sass
+ **********************************************************************/
 
 function compileSass() {
     return gulp.src('./scss/**/*.scss')
@@ -11,12 +61,14 @@ function compileSass() {
         .pipe(gulp.dest('./website/assets/css'));
 }
 
-// Watch tasks
 function watchFiles() {
     gulp.watch('./scss/**/*.scss', compileSass);
     // gulp.watch('./ts/**/*.ts', buildJS);
 }
 
-// Export tasks
+/**********************************************************************
+ Export tasks
+ **********************************************************************/
+
+export const setup = setupTasks;
 export default gulp.series(compileSass, watchFiles);
-// export const prod = gulp.series(setupTasks, productionTask);
