0fed9d7a808bccb057c4e634b1d1effc7b7a0508
ctpena <git@ctpena.net>
2025-06-14 14:10:55 +0900

    Add asagaoui-icon font handling and update card styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 import gulp from 'gulp';
 import gulpSass from 'gulp-sass';
+import * as fs from "node:fs";
 import * as dartSass from 'sass';
+import {generateFonts} from 'fantasticon';
 
 const sassCompiler = gulpSass(dartSass);
 
@@ -12,6 +14,41 @@ const sassCompiler = gulpSass(dartSass);
 /**********************************************************************
  Icon Font Generation
  **********************************************************************/
+function generateIconFonts() {
+    return generateFonts({
+        inputDir: './assets/icons/svg',
+        outputDir: './dist/icons',
+        name: 'asagaoui-icons',
+        fontTypes: ['woff2'],
+        assetTypes: ['json', 'scss'],
+        prefix: '',
+        fontsUrl: '../icons',
+        templates: {
+            scss: 'assets/icons/scss.hbs'
+        },
+        getIconId: ({basename, relativeDirPath}) => {
+            let prefix = 'ai';
+            if (relativeDirPath) {
+                if (relativeDirPath.includes('fill')) {
+                    prefix = 'aif';
+                } else if (relativeDirPath.includes('line')) {
+                    prefix = 'ail';
+                }
+            }
+            return `${prefix}-${basename}`;
+        }
+    }).then(results => {
+        gulp.src('./dist/icons/asagaoui-icons.scss')
+            .pipe(gulp.dest('./scss/'))
+            .on('end', () => {
+                fs.unlinkSync('./dist/icons/asagaoui-icons.scss');
+            });
+        return results;
+    }).catch(error => {
+        console.error('Error generating icon fonts:', error);
+        throw error;
+    });
+}
 
 /**********************************************************************
  * Default Tasks
@@ -32,4 +69,5 @@ function watchFiles() {
 /**********************************************************************
  * Export Tasks
  **********************************************************************/
+export const icons = generateIconFonts;
 export default gulp.series(compileSass, watchFiles);
